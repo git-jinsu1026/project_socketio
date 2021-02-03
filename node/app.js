6245b548a3e81f1ae61672363b9b48e49eb10b35
박진수 <parkjinsu@bagjinsuui-MacBookPro.local>
2021-02-03 23:36:20 +0900

    on , emit method

--- node/app.js
+++ node/app.js
@@ -4,10 +4,29 @@ const app = express();
 const port = 3000;
 
 
-app.get('/', ( _ ,res) => {
+app.get('/', (_, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.listen( port, () => {
+const server = app.listen(port, () => {
     console.log('Express listening on port', port);
 });
+
+//server에 소켓서버를 붙인다
+const listen = require('socket.io');
+const io = listen(server);
+//
+
+io.on('connection', (socket) => {
+
+    //console.log('소켓 서버 접속')
+    socket.on('client message', (data) => {
+
+        //console.log(data)
+        //2 전체메시지로 서버에서 뿌려주는 부분
+        io.emit('server message', {
+            message: data.message
+        })
+
+    })
+})
